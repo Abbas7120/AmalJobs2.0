39dd56a292265083e88280b4e346c0f746037f4a
Abbas <abbasfzd118@gmail.com>
2025-07-15 18:15:37 +0530

    Resolved Editor Issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-=======
+
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
->>>>>>> Improvement
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-<<<<<<< HEAD
-  plugins: [],
-=======
+
   plugins: [
     tailwindcss,
     autoprefixer,
   ],
->>>>>>> Improvement
+
 };
